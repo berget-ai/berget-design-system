3d10ea00beee96887c3ece4b03934aadf81e1c08
Christian Landgren <christian@landgren.nu>
2025-12-01 15:14:49 +0100

    feat: add subtle bokeh glow effect to card component

--- src/components/ui/Card.tsx
+++ src/components/ui/Card.tsx
@@ -1,9 +1,57 @@
 import * as React from "react";
 import { cn } from "../../utils/cn";
 
+// Bokeh background component for subtle animated glow effect
+const Bokeh = () => (
+    <div className="bokeh">
+        <div
+            className="bokeh-circle"
+            style={
+                {
+                    "--color": "rgba(96, 165, 128, 0.4)",
+                    width: "300px",
+                    height: "300px",
+                    top: "-50px",
+                    right: "-50px",
+                    animationDelay: "0s"
+                } as React.CSSProperties
+            }
+        />
+        <div
+            className="bokeh-circle"
+            style={
+                {
+                    "--color": "rgba(229, 221, 213, 0.3)",
+                    width: "250px",
+                    height: "250px",
+                    bottom: "-30px",
+                    left: "-30px",
+                    animationDelay: "-5s"
+                } as React.CSSProperties
+            }
+        />
+        <div
+            className="bokeh-circle"
+            style={
+                {
+                    "--color": "rgba(142, 178, 159, 0.3)",
+                    width: "200px",
+                    height: "200px",
+                    top: "40%",
+                    left: "60%",
+                    animationDelay: "-10s"
+                } as React.CSSProperties
+            }
+        />
+    </div>
+);
+
 const Card = React.forwardRef<HTMLDivElement, React.HTMLAttributes<HTMLDivElement>>(
-    ({ className, ...props }, ref) => (
-        <div ref={ref} className={cn("text-white", className)} {...props} />
+    ({ className, children, ...props }, ref) => (
+        <div ref={ref} className={cn("text-white relative", className)} {...props}>
+            <Bokeh />
+            <div className="relative z-10">{children}</div>
+        </div>
     )
 );
 Card.displayName = "Card";
